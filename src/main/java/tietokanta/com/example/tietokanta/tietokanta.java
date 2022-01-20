f4ffe2c3b6f0c6c18c4cb74f8c4e0a516f0843fd
eerik <flrepds@gmail.com>
2022-01-20 21:58:34 +0200

    excelin tallennus ja lataus toimii, tallennushommassa vielä viilaamista

--- src/main/java/tietokanta/com/example/tietokanta/tietokanta.java
+++ src/main/java/tietokanta/com/example/tietokanta/tietokanta.java
@@ -1,6 +1,9 @@
 package tietokanta.com.example.tietokanta;
 
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -8,19 +11,30 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 public class tietokanta extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-       TableView tableView = new TableView();
+        ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
+        List<List<String>> excelContent = new ArrayList<>();
+       TableView<ObservableList<String>> tableView = new TableView();
 
         TextField nimiKenttä = new TextField();
         TextField riviKenttä = new TextField();
@@ -29,39 +43,69 @@ public class tietokanta extends Application {
         TextField tuhkausPaikkaKenttä = new TextField();
         TextField hautausPvmKenttä = new TextField();
 
-        Workbook workbook = new HSSFWorkbook();
-        Sheet spreadsheet = workbook.createSheet("test");
+        try {
+            DataFormatter formatter = new DataFormatter();
+
+            int rowCounter = 0;
+            FileInputStream fis = new FileInputStream("testi.xlsx");
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+            XSSFSheet sheet = wb.getSheetAt(0);
+
+            for(Row row: sheet){
+                List<String> tempList = new ArrayList<>();
+                for (Cell cell:row){
+                   // CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
+                        String text = formatter.formatCellValue(cell);
+                        tempList.add(text.length() == 0 ? "":text);
+
+                }
+
+                excelContent.add(tempList);
+
+                ++rowCounter;
+                if(rowCounter == 5){
+                    break;
+                }
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
 
 
+            for(int i = 0; i <excelContent.get(0).size(); i++){
+                int curCol = i;
+                final TableColumn<ObservableList<String>, String> column = new TableColumn<>(excelContent.get(0).get(i));
+                column.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(curCol)));
+                tableView.getColumns().add(column);
 
-        TableColumn<person, String> column1 = new TableColumn<>("nimi");
-        column1.setCellValueFactory(new PropertyValueFactory<>("Name"));
-        TableColumn<person, String> column2 = new TableColumn<>("hautaustapa");
-        column2.setCellValueFactory(new PropertyValueFactory<>("hautaustapa"));
-        TableColumn<person, String> column3 = new TableColumn<>("paikka");
-        column3.setCellValueFactory(new PropertyValueFactory<>("paikka"));
-        TableColumn<person, String> column4 = new TableColumn<>("rivi");
-        column4.setCellValueFactory(new PropertyValueFactory<>("rivi"));
-        TableColumn<person, String> column5 = new TableColumn<>("tuhkauspaikka");
-        column5.setCellValueFactory(new PropertyValueFactory<>("tuhkausPaikka"));
-        TableColumn<person, String> column6 = new TableColumn<>("hautauspäivämäärä");
-        column6.setCellValueFactory(new PropertyValueFactory<>("hautausPvm"));
 
-        tableView.getColumns().addAll(column1, column2, column3, column4, column5,column6);
+            }
+        for(int i = 0; i <excelContent.size(); i++){
+            if(excelContent.get(i).contains("nimi") ||
+                    excelContent.get(i).contains("hautaustapa")||
+                    excelContent.get(i).contains("paikka")||
+                    excelContent.get(i).contains("rivi")||
+                    excelContent.get(i).contains("tuhkauspaikka")||
+                    excelContent.get(i).contains("hautauspäivämäärä")){
 
-        tableView.getItems().add(new person("paavo", "arkku", "saapasmäki", "3","derp", "2002" ));
+            }else{
+                data.add(FXCollections.observableArrayList(excelContent.get(i)));
+            }
+
+
+        }
+        tableView.setItems(data);
 
         Button addPerson = new Button("lisää");
 
-
-        Button saveToExcel = new Button("tallenna");
+        Button saveToExcel = new Button("tallenna exceliin");
         saveToExcel.setMaxSize(200, 200);
         saveToExcel.setMinHeight(100);
 
         addPerson.setOnAction(new EventHandler<ActionEvent>(){
 
             public void handle(ActionEvent event){
-
 
                 Button sendButton = new Button("lisää");
                 Button saveButton = new Button("tallenna");
@@ -77,7 +121,21 @@ public class tietokanta extends Application {
                 saveButton.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent actionEvent) {
-                       tableView.getItems().add(new person(nimiKenttä.getText(),hautaustapaKenttä.getText(),paikkaKenttä.getText(),riviKenttä.getText(),tuhkausPaikkaKenttä.getText(),hautausPvmKenttä.getText()));
+                        ObservableList<ObservableList<String>> data1 = FXCollections.observableArrayList();
+                        List<List<String>> personData = new ArrayList<>();
+                        List<String> tempList = new ArrayList<>();
+
+                        tempList.add(nimiKenttä.getText());
+                        tempList.add(hautaustapaKenttä.getText());
+                        tempList.add(paikkaKenttä.getText());
+                                tempList.add(riviKenttä.getText());
+                        tempList.add(tuhkausPaikkaKenttä.getText());
+                        tempList.add(hautausPvmKenttä.getText());
+                        personData.add(tempList);
+                        for(int i = 0; i <personData.size();i++){
+                            data.add(FXCollections.observableArrayList(personData.get(i)));
+                        }
+                      tableView.setItems(data);
                        nimiKenttä.setText("");
                        hautaustapaKenttä.setText("");
                        paikkaKenttä.setText("");
@@ -100,6 +158,99 @@ public class tietokanta extends Application {
                 newWindow.show();
 
             }});
+
+        saveToExcel.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+           public void handle(ActionEvent actionEvent) {
+                System.out.print("joo ");
+                DataFormatter formatter = new DataFormatter();
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                XSSFSheet persons = workbook.createSheet("data");
+               // XSSFRow row;
+
+        Row row = persons.createRow(0);
+              //  Map<String, Object[]> dataToBeSaved = new TreeMap<String, Object[]>();
+
+                for(int i = 0; i < tableView.getColumns().size(); i++){
+                    row.createCell(i).setCellValue(tableView.getColumns().get(i).getText());
+
+
+                 /*   int curCol = 0;
+
+                    dataToBeSaved.put("1", new Object[]{tableView.getColumns().get(curCol).getText(),
+                            (curCol++),
+                            tableView.getColumns().get(curCol).getText(),
+                            (curCol++),
+                                    tableView.getColumns().get(curCol).getText(),
+                            (curCol++),
+                                          tableView.getColumns().get(curCol).getText(),
+                            (curCol++),
+                                                   tableView.getColumns().get(curCol).getText(),
+                            (curCol++),
+                                                          tableView.getColumns().get(curCol).getText()});*/
+
+
+
+                }
+
+                    for(int i = 0; i<tableView.getItems().size(); i++) {
+
+                        row = persons.createRow(i+1);
+                        for(int j = 0; j <tableView.getColumns().size();j++)
+                        {
+                            if(tableView.getColumns().get(j).getCellData(i) != null){
+                                row.createCell(j).setCellValue(tableView.getColumns().get(j).getCellData(i).toString());
+                            }else{
+                                row.createCell(j).setCellValue("");
+                            }
+                        }
+                    /*    dataToBeSaved.put("2", new Object[]{
+                                tableView.getItems().get(0).get(i)});
+                      //  System.out.print(tableView.getColumns().get(curCol).getCellData(i));
+                      //  System.out.print("curcol "+ curCol);
+                      //  System.out.print(tableView.getItems().get(0).size());
+                        System.out.print(tableView.getItems().get(0).get(i));*/
+
+                    }
+
+
+
+         /*   Set<String> keyId = dataToBeSaved.keySet();
+
+            int rowId = 0;
+                for (String key: keyId)
+            {
+
+                row = persons.createRow(rowId++);
+                Object[] objectArr = dataToBeSaved.get(key);
+
+                int cellId = 0;
+                for(Object obj:objectArr){
+                    Cell cell = row.createCell(cellId++);
+
+                    cell.setCellValue(obj.toString());
+                }
+
+            }*/
+                try {
+                    FileChooser chooser = new FileChooser();
+                    chooser.setTitle("open");
+                    chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("text","*txt"));
+                    File selectedFile = chooser.showSaveDialog(stage);
+                    if(selectedFile != null) {
+
+                    }
+                    FileOutputStream out = new FileOutputStream(new File(String.valueOf(selectedFile)));
+                    System.out.print("filessä");
+                    workbook.write(out);
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
 
        VBox vbox = new VBox(tableView);
         VBox vbox2 = new VBox(0);
